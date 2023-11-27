d0cffbdf70638d3963c9a303e6de64b813a4341c
marugervasoni <gervasonimaria15@gmail.com>
2023-11-27 16:35:41 -0300

    estructura proyecto diseño orientado a paquetes

---
+++ internal/domain/products.go
@@ -0,0 +1,14 @@
+package main
+
+import	"time"
+
+//product struct
+type Product struct {
+	Id          string    `json:"id"`
+	Name        string    `json:"name"`
+	Quantity    int       `json:"quantity"`
+	CodeValue   string    `json:"code_value"`
+	IsPublished bool      `json:"is_published"`
+	Expiration  time.Time `json:"expiration"`
+	Price       float64   `json:"price"`
+}
